847a340f04cd0936218796b5928beabd457caf28
Muhammad Lawal Adamu <lawaldd@gmail.com>
2023-04-30 12:27:03 +0100

    update: add custom classes to tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,18 @@ module.exports = {
   mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        zeedas: {
+          'border-gray': '##D8DAE5',
+          'menu-bg-10': '#1018401A',
+          'text-blue': '#23B3E8',
+        },
+      },
+      boxShadow: {
+        zeedas: '0px 4px 15px rgba(0, 0, 0, 0.1)',
+      },
+    },
   },
   plugins: [],
 }
